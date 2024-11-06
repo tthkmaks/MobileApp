8c7e6afd7bfbb23dd2a0399d530f0c88d67ae76f
opilane <opilane@HP-CZC0359NNV>
2024-11-06 12:39:26 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MaxSiCalcilatoor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MaxSiCalcilatoor
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Cairo-Light.ttf", "RegularFront");
+                    fonts.AddFont("Cairo-ExtraLight.ttf", "LightFront");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
